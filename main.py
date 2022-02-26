a9b20b1095bbe45c7bccb8172ceb47cea841d66c
victoryes7 <victoryes7@naver.com>
2022-02-26 22:18:52 +0900

    version 1.0

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-print(f'{"microdust project v 1.0":=^100}')
-print('='*100)
+print(f'{"microdust project v 1.0":=^70}')
+print('='*70)
 
 import requests
 import json
+
+stationname = input('input your needed station name:')
 
 REQUESTPOINT = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'
 PAR = {
@@ -10,10 +12,11 @@ PAR = {
            ,'returnType' : 'json'
            ,'numOfRows' : '100'
            ,'pageNO' : '1'
-           ,'stationName' : '종로구'
+           ,'stationName' : False
            ,'dataTerm' : 'DAILY'
            ,'ver' : '1.0'           
 }
+PAR['stationName'] = stationname
 
 ANSWER = requests.get(REQUESTPOINT,params = PAR)
 RAWDATADIC = json.loads(ANSWER.content)
@@ -27,5 +30,40 @@ if ddatastate != '00' :
 
 DATALIST = RAWDATADIC['response']['body']['items']
 DATAamou = RAWDATADIC['response']['body']['totalCount']
+DATAdate = RAWDATADIC['response']['body']['items'][0]['dataTime']
+DATAdate = DATAdate[:10]
 
-gitcheck = 3
+ListOfCrit = [0.15 , 25 , 0.1 , 0.1 , 100 , 35]
+#아황산, 일산화탄소, 오존, 이산화질소, 미세먼지, 초미세먼지
+#SO2, CO, O3, NO2, PM10, PM25, 단위는 ppm, microgram/m세제곱
+RFdatadic = {}
+for Q in range(DATAamou) :
+      RFdatadic[Q] = []
+
+for num , DATA in enumerate(DATALIST) :
+      ListOfDATA = [0 for x in range(7)]
+      
+      ListOfDATA.append(DATA.get('so2Value' , 0))
+      ListOfDATA.append(DATA.get('coValue' , 0))
+      ListOfDATA.append(DATA.get('o3Value' , 0))
+      ListOfDATA.append(DATA.get('no2Value' , 0))
+      ListOfDATA.append(DATA.get('pm10Value' , 0))
+      ListOfDATA.append(DATA.get('pm25Value' , 0))
+      
+      for Q in range(6) :
+            if ListOfDATA[Q] > ListOfCrit[Q] :
+                  RFdatadic[num].append(Q)
+                  
+MSG = f'air quality of {DATAdate}'
+print(f'{MSG:=^70}')
+
+AtmoFact = ['sulfurate' , 'carbon monoxide' , 'ozone' , 'nitrogen dioxide' , 'fine dust' , 'ultrafine dust']
+for Q in range(DATAamou) :
+      
+      DATAtime = RAWDATADIC['response']['body']['items'][Q]['dataTime'][11:]
+      print(f'{DATAtime:=^70}')
+      
+      if RFdatadic[Q] :
+            for W in RFdatadic[Q] :
+                  print(f'{AtmoFact[W]}')
+                  
